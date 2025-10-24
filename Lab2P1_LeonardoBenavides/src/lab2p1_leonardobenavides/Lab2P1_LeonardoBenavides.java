092ac4a1d7487fa044ccce03c0391ab4ff33bb73
LeoB <leobenavides2007@gmail.com>
2025-10-24 15:59:02 -0600

    Primer programa hecho

--- Lab2P1_LeonardoBenavides/src/lab2p1_leonardobenavides/Lab2P1_LeonardoBenavides.java
+++ Lab2P1_LeonardoBenavides/src/lab2p1_leonardobenavides/Lab2P1_LeonardoBenavides.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab2p1_leonardobenavides;
-
+import java.util.Scanner;
 /**
  *
  * @author benav
@@ -14,7 +14,87 @@ public class Lab2P1_LeonardoBenavides {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    Scanner leer = new Scanner(System.in);
+    char continuar = 's';
+    while (continuar == 's'|| continuar == 'S'){
+        System.out.println(" MENU ");
+        System.out.println(" 1. Hola Mundo ");
+        System.out.println(" 2. Protege tu Nota ");
+        System.out.println(" 3. Sumatoria de cuadrados ");
+        System.out.println(" Eliga una opcion: ");
+        int opcion = leer.nextInt();
+        if (opcion == 1){
+            System.out.println(" Ingrese un numero mayor que 1 y menor que 100: ");
+            int numero = leer.nextInt();
+            if (numero>=1 && numero<100){
+                int contador = 0;
+                while (contador<numero){
+                   
+                    
+                    contador = contador + 1;
+                    if (contador%3==0 && contador%5==0){
+                        System.out.println("HolaMundo");
+                    }
+                    else
+                        if(contador%3==0){
+                            System.out.println("Hola");
+                        }
+                    else
+                       if(contador%5==0 ) {
+                           System.out.println("Mundo");
+                       }
+                        else{
+                           System.out.println(contador);
+                       }
+                }
+                
+                    
+               }
+               
+            
+                    
+                
+            }// fin opcion 1
+            else
+                if(opcion == 2){
+                   int contador_turnos = 5;
+                   int presa_vidas = 3;
+                   while (contador_turnos>0){
+                       System.out.println("Intentos restantes " + contador_turnos);
+                       System.out.println("PRESA");
+                       System.out.println("Elige un lugar en el que esconderte");
+                       System.out.println("1. - Tu cuarto");
+                       System.out.println("2. - La cocina");
+                       System.out.println("3. - El bano ");
+                       
+                       int opcion_presa = leer.nextInt();
+                       
+                       System.out.println("EL CAZADOR");
+                       System.out.println("1. - El cuarto");
+                       System.out.println("2. - La cocina ");
+                       System.out.println("3. - El bano"); 
+                       
+                       int opcion_cazador = leer.nextInt();
+                       
+                       if (opcion_presa ==  opcion_cazador);{
+                          System.out.println("HAS SIDO ENCONTRADO! ");
+                          contador_turnos = contador_turnos - 1;
+                   }
+                       
+                       
+                   }
+                   
+                    
+                }
+            
+        System.out.println("Desea continuar? S/N");
+        continuar = leer.next().charAt(0);}// fin del menu
+    }// fin de clase
+       
+        
+        
+        
+        
+    }// fin del main
     
-}
+
